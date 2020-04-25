60588989c779ef3b0fc06e65a45d8b6fee2da2d9
Yong-Lu Li <lylxiaonei@163.com>
2020-04-25 19:29:17 +0800

    Update download_dataset.sh

--- script/download_dataset.sh
+++ script/download_dataset.sh
@@ -44,9 +44,11 @@ cd ../../
 # ---------------HICO-DET Dataset------------------
 echo "Downloading HICO-DET Dataset"
 
-URL_HICO_DET=http://napoli18.eecs.umich.edu/public_html/data/hico_20160224_det.tar.gz
+# URL_HICO_DET=http://napoli18.eecs.umich.edu/public_html/data/hico_20160224_det.tar.gz
+# new url hico-det from their website: https://drive.google.com/open?id=1QZcJmGVlF9f4h-XLWe9Gkmnmj2z1gSnk
 
-wget -N $URL_HICO_DET -P Data/
+# wget -N $URL_HICO_DET -P Data/
+python script/Download_data.py 1QZcJmGVlF9f4h-XLWe9Gkmnmj2z1gSnk Data/hico_20160224_det.tar.gz
 tar -xvzf Data/hico_20160224_det.tar.gz -C Data/
 rm Data/hico_20160224_det.tar.gz
 
